e3a444426aa1f0d92356b1f6ea5834557722ad29
tusharbhandarii <15tusharx2@gmail.com>
2024-09-13 12:56:05 +0530

    updating for the negative value input

--- conditional-loops/Gcd.java
+++ conditional-loops/Gcd.java
@@ -5,17 +5,22 @@ public class Gcd {
 
         System.out.print("enter the first number: ");
         int num1 = obj.nextInt();
+        // for absolite value
+        int absNum1 = (num1<0)? -num1 : num1;
+        
         System.out.print("Enter the second numbar: ");
         int num2 = obj.nextInt();
+        // for absolite value
+        int absNum2 = (num2<0)? -num2 : num2;
 
-        while(num1>0 && num2>0){
-            if(num1>num2){
-                num1%=num2;
+        while(absNum1>0 && absNum2>0){
+            if(absNum1>absNum2){
+                absNum1%=absNum2;
             }else{
-                num2%=num1;
+                absNum2%=absNum1;
             }
         }
-        int gcd = (num1>num2)? num1:num2;
+        int gcd = (absNum1>absNum2)? absNum1:absNum2;
 
         System.out.println(gcd);
         obj.close();
